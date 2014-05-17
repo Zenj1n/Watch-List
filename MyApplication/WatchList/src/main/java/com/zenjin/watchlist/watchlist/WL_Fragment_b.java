2f08d6c7bf8235a6997252954a4021061241e4b8
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2014-05-17 13:31:18 +0200

    added fragments

---
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/WL_Fragment_b.java
@@ -0,0 +1,33 @@
+package com.zenjin.watchlist.watchlist;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ *
+ */
+public class WL_Fragment_b extends Fragment {
+
+
+    public WL_Fragment_b() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b_wl, container, false);
+    }
+
+
+}
