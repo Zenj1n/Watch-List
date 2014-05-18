3699f3b4d6376f4079459f40997f6cb36b7abec2
Jtlie <jtlie@live.nl>
2014-05-18 19:01:10 +0200

    ???

--- MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/MainActivity.java
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/MainActivity.java
@@ -1,23 +1,53 @@
 package com.zenjin.watchlist.watchlist;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    public String username = "test";
+    public String password = "test123";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button LogIn=(Button)findViewById(R.id.LogIn);
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText usernameInput = (EditText) findViewById(R.id.Username);
+                EditText passwordInput = (EditText) findViewById(R.id.Password);
+
+                if(username == usernameInput.getText().toString() && password == passwordInput.getText().toString()){
+                    Toast.makeText(getApplicationContext(), "Correct",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong Username or Password!",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
