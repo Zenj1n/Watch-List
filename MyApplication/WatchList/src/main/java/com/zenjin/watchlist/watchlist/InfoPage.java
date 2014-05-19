1a25043ecbfa8eb8395b553ee616ca6cf90ee442
Falio <f.fremouw@gmail.com>
2014-05-19 14:49:14 +0200

    popupmenu

--- MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/InfoPage.java
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/InfoPage.java
@@ -3,15 +3,33 @@ package com.zenjin.watchlist.watchlist;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 
 public class InfoPage extends ActionBarActivity {
+
+    Button Baddto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infopage);
+
+        Baddto = (Button) findViewById(R.id.Baddto);
+        Baddto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(InfoPage.this, Baddto);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.popup_menu, popup.getMenu());
+                popup.show();
+            }
+        });
     }
 
 
