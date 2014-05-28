703e1e31e506fea767467b4161a017a8113dbd07
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2014-05-28 10:58:41 +0200

    ?

--- MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/WL_Fragment_a.java
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/WL_Fragment_a.java
@@ -54,7 +54,7 @@ public class WL_Fragment_a extends Fragment {
         mListView = getListView();
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> av, View view, int i, long l) {
-                
+
                 Toast.makeText(getActivity(), "Positie "+i , Toast.LENGTH_SHORT).show();
 
                 if (i == 10){
