394d6e8f9c272d7550cd2b4b08d0e24c6f2867c7
Falio <f.fremouw@gmail.com>
2014-05-28 10:08:34 +0200

    files gefixed

--- MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/Hm_MainActivity.java
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/Hm_MainActivity.java
@@ -13,6 +13,6 @@ public class Hm_MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.hm_activity);
     }
 }
