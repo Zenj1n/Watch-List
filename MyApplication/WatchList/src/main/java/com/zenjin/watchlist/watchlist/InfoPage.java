0f8c27e86dad5518be8d3961446ec3d8c2104ce2
Jtlie <jtlie@live.nl>
2014-06-08 19:56:48 +0200

    Aan list toevoegen

--- MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/InfoPage.java
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/InfoPage.java
@@ -1,16 +1,13 @@
 package com.zenjin.watchlist.watchlist;
 
-import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.os.StrictMode;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,20 +16,14 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
-import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
-import com.parse.ParseQuery;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
-import java.net.URL;
-import java.text.ParseException;
-import java.util.List;
 
 
 public class InfoPage extends ActionBarActivity {
@@ -79,11 +70,11 @@ public class InfoPage extends ActionBarActivity {
                         public boolean onMenuItemClick(MenuItem menuItem) {
                             switch (menuItem.getItemId()) {
                                 case R.id.watching:
-                                    //ParseObject watching = new ParseObject("Koppel");
-                                    //watching.put("User", ParseUser.getCurrentUser());
-                                    //watching.put("Serie",  Title.getText());
-                                    //watching.put("Status", "Watching");
-                                    //watching.saveInBackground();
+                                    /*ParseObject watching = new ParseObject("Koppel");
+                                    watching.put("User", ParseUser.getCurrentUser());
+                                    watching.put("Serie",  Title.getText());
+                                    watching.put("Status", "Watching");
+                                    watching.saveInBackground();
 
                                     /*
                                     ParseQuery<ParseObject> query = ParseQuery.getQuery("Koppel");
@@ -92,9 +83,9 @@ public class InfoPage extends ActionBarActivity {
                                     query.findInBackground(new FindCallback<ParseObject>()  {
                                         public void done (List<ParseObject> User, ParseException e) {
                                             if (e == null) {
-                                                Log.d("score", "Retrieved " + User.size() + " scores");
+
                                             } else {
-                                                Log.d("score", "Error: " + e.getMessage());
+
                                             }
                                         }
                                     });
