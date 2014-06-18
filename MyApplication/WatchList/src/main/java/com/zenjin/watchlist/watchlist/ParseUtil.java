e1e645d2fbe60f38c95aeccbe924df8219029f1d
Jtlie <jtlie@live.nl>
2014-06-18 10:40:25 +0200

    rating, add to page, status, etc

---
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/ParseUtil.java
@@ -0,0 +1,15 @@
+package com.zenjin.watchlist.watchlist;
+
+/**
+ * Created by Jason on 18-6-2014.
+ */
+public class ParseUtil {
+    public static final String USER = "User";
+    public static final String SERIE = "Serie";
+    public static final String RATING = "Rating";
+    public static final String KOPPEL = "Koppel";
+    public static final String STATUS = "Status";
+    public static final String COMPLETED = "Completed";
+    public static final String PLAN_TO_WATCH = "Plan to watch";
+    public static final String WATCHING = "Watching";
+}
