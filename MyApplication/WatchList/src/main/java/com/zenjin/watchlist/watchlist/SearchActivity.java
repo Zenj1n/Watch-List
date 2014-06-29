ae24ca8cf1f869add8e98cc2c0f7ef318d6176f2
Hoye <hoyelam96@gmail.com>
2014-06-29 15:39:09 +0200

    Search

--- MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/SearchActivity.java
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/SearchActivity.java
@@ -1,174 +1,127 @@
 package com.zenjin.watchlist.watchlist;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.parse.Parse;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 
 
 public class SearchActivity extends Activity {
 
-    private EditText searchET;
-    private EditText userET;
-    private Intent intent;
+    ListView list;
+    TextView showTitle;
+    TextView overview;
+    TextView api;
+    Button Btngetdata;
+    EditText searchShowET;
+    ArrayList<HashMap<String, String>> searchlist = new ArrayList<HashMap<String, String>>();
+    private static String url = "http://api.trakt.tv/search/shows/390983740f2092270bc0fa267334db88/20140627/the+arrow";
+
     private static final String TAG_TITLE = "title";
-    private List<String> searchResults = new ArrayList<String>();
+    private static final String TAG_OVERVIEW = "overview";
+    private static final String TAG_POSTER = "poster";
+
+    JSONArray searchResult = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        searchlist = new ArrayList<HashMap<String, String>>();
 
-        ActionBar actionBar = getActionBar();
-
-        // Enabling Back navigation on Action Bar icon
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        handleIntent(getIntent());
-
-        Parse.initialize(this, "cbrzBhn5G4akqqJB5bXOF6X1zCMfbRQsce7knkZ6", "Z6VQMULpWaYibP77oMzf0p2lgcWsxmhbi8a0tIs6");
-
-        userET = (EditText) findViewById(R.id.searchUserET);
-        searchET = (EditText) findViewById(R.id.SearchET);
-
-
-
-        searchET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        searchShowET = (EditText) findViewById(R.id.searchShowET);
+        searchShowET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    performSearch();
-                    return true;
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                    new JSONParse().execute();
                 }
-                return false;
-            }
-            public void performSearch(){
-                new JSONParse().execute();
+
+                return true;
             }
         });
-
-       userET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-           @Override
-           public boolean onEditorAction(TextView v, int actionId, KeyEvent keyEvent) {
-               if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                   performUserSearch();
-                   return true;
-               }
-               return false;
-           }
-           public void performUserSearch() {
-               Toast.makeText(getApplicationContext(), "Feature not available yet", Toast.LENGTH_SHORT).show();
-               InputMethodManager imm = (InputMethodManager)getSystemService(
-                       Context.INPUT_METHOD_SERVICE);
-               imm.hideSoftInputFromWindow(userET.getWindowToken(), 0);
-           }
-       });
-
-
     }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            Log.d("hallo", query);
-            new JSONParse().execute(query);
-        }
-    }
-
-    public class JSONParse extends AsyncTask<String, String, JSONArray> {
+    private class JSONParse extends AsyncTask<String, String, JSONArray> {
         private ProgressDialog pDialog;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-
+            showTitle = (TextView) findViewById(R.id.showTitle);
+            overview = (TextView) findViewById(R.id.overview);
+            //poster = (Imageview)findViewById(R.id.poster);
             pDialog = new ProgressDialog(SearchActivity.this);
             pDialog.setMessage("Getting Data ...");
             pDialog.setIndeterminate(false);
-            pDialog.setCancelable(false);
+            pDialog.setCancelable(true);
             pDialog.show();
+
         }
 
-
-
-        @Override
-        protected JSONArray doInBackground(String... query) {
-
-
-            //String word2 = searchET.getText().toString();
-            //String searchWord = word2.replaceAll(" ", "+");
-            String query1 =  Arrays.toString(query);
-            String querySearch = query1.replace("[","");
-            String urlSearch = "http://api.trakt.tv/search/shows.json/2c0bdfbdb92cb55e844c997757180341?query="+ querySearch;
+        protected JSONArray doInBackground(String... args) {
             ServiceHandler jParser = new ServiceHandler();
+            JSONArray jsonSearch = jParser.getJsonArray(url);
+            JSONArray jsonArray = new JSONArray();
+            jsonArray.put(jsonSearch);
 
-            // Getting JSON from URL
-            JSONArray jsonSearch = jParser.getJsonArray(urlSearch);
-            return jsonSearch;
-
+            return jsonArray;
         }
 
-        @Override
-        protected void onPostExecute(JSONArray jsonSearch) {
+        protected void onPostExecute(JSONArray jsonArray) {
             pDialog.dismiss();
             try {
-                DisplayImageOptions options = new DisplayImageOptions.Builder()
-                        .cacheOnDisk(true)
-                        .cacheInMemory(true)
-                        .build();
-                // Storing  JSON item in a Variable
+                JSONArray jsonSearch = jsonArray.getJSONArray(0);
+                for (int i = 0; i < jsonSearch.length(); i++) {
+                    JSONObject e;
+                    e = jsonSearch.getJSONObject(i);
+                    String name = e.getString("title");
+                    String overview = e.getString("overview");
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    map.put(TAG_TITLE, name);
+                    map.put(TAG_OVERVIEW, overview);
+                    searchlist.add(map);
+                    list = (ListView) findViewById(R.id.list);
+                    ListAdapter adapter = new SimpleAdapter(SearchActivity.this, searchlist,
+                            R.layout.search_listview,
+                            new String[]{TAG_TITLE, TAG_OVERVIEW}, new int[]{
+                            R.id.showTitle, R.id.overview}
+                    );
+                    list.setAdapter(adapter);
+                    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view,
+                                                int position, long id) {
+                            Toast.makeText(SearchActivity.this, "You Clicked at " + searchlist.get(+position).get("name"), Toast.LENGTH_SHORT).show();
 
-                if(jsonSearch != null){
-                    for(int i=0;i<jsonSearch.length();i++){
-                        JSONObject s;
-                        s = jsonSearch.getJSONObject(i);
-                        String test1 = s.getString(TAG_TITLE);
-                        String title = test1.replace("[", "");
-                        searchResults.add(title);
-                    }
-                    System.out.println(searchResults);
+                        }
+                    });
                 }
-                else{
-                    Toast.makeText(getApplicationContext(), "No Information Available", Toast.LENGTH_SHORT).show();
 
-                }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
