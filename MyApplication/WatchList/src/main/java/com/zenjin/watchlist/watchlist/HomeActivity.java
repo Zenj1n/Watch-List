d73a74a0431d56eab1a0542ceffd85b72215377b
Hoye <hoyelam96@gmail.com>
2014-06-30 14:09:26 +0200

    doe gwn ieks

--- MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/HomeActivity.java
+++ MyApplication/WatchList/src/main/java/com/zenjin/watchlist/watchlist/HomeActivity.java
@@ -251,9 +251,6 @@ public class HomeActivity extends MyWatchList {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         if (id == R.id.action_search) {
             Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
             startActivity(intent);
